31a0c4240bd69e8647a2f03d9d695a9c10fcaa46
Kanon <mkc35r.0@gmail.com>
2024-04-13 20:16:23 +0900

    note完成

--- dev/reactProject/noteapp-with-react/src/App.jsx
+++ dev/reactProject/noteapp-with-react/src/App.jsx
@@ -1,11 +1,57 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Main from './components/Main'
 import Sidebar from './components/Sidebar'
+import uuid from 'react-uuid'
 
 function App() {
+  const [notes,setNotes] = useState(JSON.parse(localStorage.getItem('notes')) || [])
+  const [activeNote, setActiveNote] = useState(false)
+
+  useEffect(() => {
+    localStorage.setItem('notes',JSON.stringify(notes))},
+    [notes]);
+
+    useEffect(() => {
+      setActiveNote(notes[0].id) },
+      []);
+
+  const onAddNote = () =>{
+    console.log('新しくノートが追加されました')
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content:"新しいノートの内容",
+      modDate: Date.now(),
+    }
+    setNotes([...notes,newNote])
+    console.log(notes)
+  }
+
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id)
+    setNotes(filterNotes)
+  }
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote)
+  }
+
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArray = notes.map((note) => {
+      if(note.id === updatedNote.id){
+        return updatedNote
+      }else{
+        return note
+      }
+    })
+    setNotes(updatedNotesArray)
+  }
+
   return <div className='App'>
-    <Sidebar />
-    <Main />
+    <Sidebar onAddNote={onAddNote} notes={notes} onDeleteNote={onDeleteNote} activeNote={activeNote}
+    setActiveNote={setActiveNote}/>
+    <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote}/>
   </div>   
 }
 
--- dev/reactProject/noteapp-with-react/src/components/Sidebar.jsx
+++ dev/reactProject/noteapp-with-react/src/components/Sidebar.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import './Sidebar.css'
 
-const Sidebar = () => {
+const Sidebar = ({onAddNote, notes, onDeleteNote,activeNote,setActiveNote}) => {
+  
+  const sortedNotes = notes.sort((a,b) => b.modDate - a.modDate)
+  
   return (
     <div className='app-sidebar'>
       <div className='app-sidebar-header'>
         <h1>ノート</h1>
-        <button>追加</button>
+        <button onClick={onAddNote}>追加</button>
       </div>
       <div className='app-sidebar-notes'>
-        <div className='app-sidebar-note'>
+        {sortedNotes.map((note) => (
+          <div className={`app-sidebar-note ${note.id === activeNote && 'active'}`} key={note.id} onClick={() => setActiveNote(note.id)}>
           <div className='sidebar-note-title'>
-            <strong>タイトル</strong>
-            <button>削除</button>
+            <strong>{note.title}</strong>
+            <button onClick={() => onDeleteNote(note.id)}>削除</button>
           </div>
-          <p>ノートの内容です</p>
-          <small>最後の修正日：XX/XX</small>
+          <p>{note.content}</p>
+          <small>{new Date(note.modDate).toLocaleDateString("ja-JP",{hour:'2-digit', minute:'2-digit'})}</small>
         </div>
+        ))}
       </div>
     </div>
   )
