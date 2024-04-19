da3ee035d2872eae83b60e0349327b4a7f786666
Kanon <mkc35r.0@gmail.com>
2024-04-19 12:28:00 +0900

    全体のレイアウトを作成

---
+++ udemy/udemy-household-app/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Report from './pages/Report';
+import Home from './pages/Home';
+import NoMatch from './pages/NoMatch';
+import AppLayout from './components/layout/AppLayout';
+import {theme} from './theme/theme'
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <Router>
+        <Routes>
+          <Route path='/' element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path='/report' element={<Report />} />
+            <Route path='*' element={<NoMatch />} />
+          </Route>
+          </Routes>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
